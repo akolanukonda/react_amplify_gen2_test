9b362f10eb920f5f96fd08030b88a70b46ae7c74
Pravallika Lakshmi Raga Vutukuri <pvutukuri@deltaintech.in>
2024-07-22 19:05:22 +0530

    Added authenticator

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Authenticator } from '@aws-amplify/ui-react'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <Authenticator>
+            {({ signOut, user }) => (
+             
+    <main>
+                <h1>{user?.signInDetails?.loginId}'s todos</h1>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -28,7 +33,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </main>
+            )}
+    </Authenticator>
   )
 }
 
